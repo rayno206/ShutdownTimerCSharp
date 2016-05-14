6d679b227197f1a332af259e00e0c6d3799ab345
Patrick Mai <rayno206@gmail.com>
2016-05-14 10:20:19 -0700

    Completed

---
+++ C#/ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ShutdownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtHour = new System.Windows.Forms.NumericUpDown();
+            this.txtMinute = new System.Windows.Forms.NumericUpDown();
+            this.txtSecond = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.txtHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSecond)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(47, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Designed by Patrick Mai";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(133, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(70, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(147, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "SS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(84, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "MM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(21, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "HH";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnCancel.Location = new System.Drawing.Point(196, 96);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(61, 21);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click_1);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnStart.Location = new System.Drawing.Point(196, 54);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(61, 21);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(24, 70);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(40, 20);
+            this.txtHour.TabIndex = 0;
+            this.txtHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.Location = new System.Drawing.Point(87, 70);
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(40, 20);
+            this.txtMinute.TabIndex = 1;
+            this.txtMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(150, 70);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(40, 20);
+            this.txtSecond.TabIndex = 2;
+            this.txtSecond.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(285, 153);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtMinute);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shutdown Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSecond)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown txtHour;
+        private System.Windows.Forms.NumericUpDown txtMinute;
+        private System.Windows.Forms.NumericUpDown txtSecond;
+    }
+}
+
---
+++ C#/ShutdownTimer/Form1.cs
@@ -0,0 +1,145 @@
+﻿
+/*
+    Author: Patrick Thanh Phuong Mai
+    Email: rayno206@gmail.com
+    GitHub: https://github.com/rayno206
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace ShutdownTimer
+{
+    public partial class Form1 : Form
+    {
+        public static int timeLeft;
+        public static int hour;
+        public static int min;
+        public static int sec;
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000; // Time Interval
+            timer.Tick += new EventHandler(timer_Tick); //Raising Event on each tick
+            btnStart.Enabled = true;
+            btnCancel.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Timing Shutdown");
+            comboBox1.Items.Add("Timing Restart");
+            comboBox1.Items.Add("Timing Sleep");
+            comboBox1.SelectedItem = "Timing Shutdown";
+            btnStart.Enabled = true;
+            btnCancel.Enabled = false;
+            txtHour.Text = "00";
+            txtMinute.Text = "60";
+            txtSecond.Text = "00";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.Equals("Timing Shutdown"))
+            {
+                if (timeLeft > 0)
+                {
+                    timeLeft = timeLeft - 1; // decrement of timeleft on each tick
+                    hour = timeLeft / 3600; // Left hours
+                    min = (timeLeft - (hour * 3600)) / 60; //Left Minutes
+                    sec = timeLeft - (hour * 3600) - (min * 60); //Left Seconds
+                    txtHour.Text = hour.ToString(); // Setting hour Text on each timer tick
+                    txtMinute.Text = min.ToString(); // Setting minutes Text on each timer tick
+                    txtSecond.Text = sec.ToString(); // Setting sec Text on each timer tick
+                }
+                else
+                {
+                    timer.Stop(); // Stop Timer
+                    Process.Start("shutdown", "/s /t 0"); // Shutdown PC when Time is over
+                }
+            }
+            else if (comboBox1.SelectedItem.Equals("Timing Restart"))
+            {
+                if (timeLeft > 0)
+                {
+                    timeLeft = timeLeft - 1; // decrement of timeleft on each tick
+                    hour = timeLeft / 3600; // Left hours
+                    min = (timeLeft - (hour * 3600)) / 60; //Left Minutes
+                    sec = timeLeft - (hour * 3600) - (min * 60); //Left Seconds
+                    txtHour.Text = hour.ToString(); // Setting hour Text on each timer tick
+                    txtMinute.Text = min.ToString(); // Setting minutes Text on each timer tick
+                    txtSecond.Text = sec.ToString(); // Setting sec Text on each timer tick
+                }
+                else
+                {
+                    timer.Stop(); // Stop Timer
+                    Process.Start("shutdown", "/r /t 0"); // Restart PC when Time is over
+                }
+            }
+            else if (comboBox1.SelectedItem.Equals("Timing Sleep"))
+            {
+                if (timeLeft > 0)
+                {
+                    timeLeft = timeLeft - 1; // decrement of timeleft on each tick
+                    hour = timeLeft / 3600; // Left hours
+                    min = (timeLeft - (hour * 3600)) / 60; //Left Minutes
+                    sec = timeLeft - (hour * 3600) - (min * 60); //Left Seconds
+                    txtHour.Text = hour.ToString(); // Setting hour Text on each timer tick
+                    txtMinute.Text = min.ToString(); // Setting minutes Text on each timer tick
+                    txtSecond.Text = sec.ToString(); // Setting sec Text on each timer tick
+                }
+                else
+                {
+                    timer.Stop(); // Stop Timer
+                    SetSuspendState(false, true, true); // Shutdown PC when Time is over
+                }
+            }
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            hour = Int16.Parse(txtHour.Text);
+            min = Int16.Parse(txtMinute.Text);
+            sec = Int16.Parse(txtSecond.Text);
+            timeLeft = hour * 3600 + min * 60 + sec;
+            timer.Start();
+            btnStart.Enabled = false;
+            btnCancel.Enabled = true;
+            txtHour.Enabled = false;
+            txtMinute.Enabled = false;
+            txtSecond.Enabled = false;
+            comboBox1.Enabled = false;
+        }
+
+        private void btnCancel_Click_1(object sender, EventArgs e)
+        {
+            hour = 0;
+            min = 0;
+            sec = 0;
+            timeLeft = 0;
+            txtHour.Text = "00";
+            txtMinute.Text = "60";
+            txtSecond.Text = "00";
+            timer.Stop();
+            btnStart.Enabled = true;
+            btnCancel.Enabled = false;
+            txtHour.Enabled = true;
+            txtMinute.Enabled = true;
+            txtSecond.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+    }
+}
